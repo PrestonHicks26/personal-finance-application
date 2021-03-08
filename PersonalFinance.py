8b2bb9f4c9d2a4c4b7009494b40e8d4f54d3994f
PrestonHicks26 <78393706+PrestonHicks26@users.noreply.github.com>
2021-03-08 14:50:36 -0500

    Add files via upload
    Defined possible methods for Finance class based on requirements doc

---
+++ PersonalFinance.py
@@ -0,0 +1,67 @@
+class Finance:
+    def __init__(self):
+        pass
+
+    def singularIncome(self):
+        pass
+
+    def addRecurringIncome(self):
+        pass
+
+    def removeRecurringIncome(self):
+        pass
+
+
+    def singularExpense(self):
+        pass
+
+    def addRecurringExpense(self):
+        pass
+
+    def removeRecurringExpense(self):
+        pass
+
+    #ie. divide income into different categories
+    def setBudget(self):
+        pass
+
+    def addBudgetCategory(self):
+        pass
+
+    def removeBudgetCategory(self):
+        pass
+
+
+    #ie. gift card, bank account, paper money, ect.
+    #find better method name
+    def addMoneyType(self):
+        pass
+
+
+    def addGoal(self):
+        pass
+
+    def removeGoal(self):
+        pass
+
+    def checkProgress(self):
+        pass
+
+
+    def sendAlert(self, alertName):
+        if alertName == "":
+            pass
+        if alertName == "":
+            pass
+
+
+    def save(self):
+        pass
+
+    def getSave(self):
+        pass
+
+
+    @staticmethod
+    def checkDate(self):
+        pass
