fed13abbabf5872b91ccafb4b97eb8143293b1c5
Preston L. Hicks <hicksp@rider.edu>
2021-04-16 16:58:59 -0400

    Updated files
    I write two new classes: one for recurring incomes, and one for recurring expenses. I worked on the income and expense methods, along with the budget methods. Finally, I worked on the saveAs method.

--- PersonalFinance.py
+++ PersonalFinance.py
@@ -1,15 +1,27 @@
 from os import path
 
+class Recurring_Income:
+    def __init__(self, moneyType, date, frequency):
+        self.moneyType = moneyType
+        self.date = date  # starting date for recurring income
+        self.frequency = frequency  # how often income recurs
+
+class Recurring_Expense:
+    def __init__(self, moneyType, date, frequency):
+        self.moneyType = moneyType
+        self.date = date
+        self.frequency = frequency
+
 class Finance:
     def __init__(self):
         self.program_state = ""
         self.lastSave = ""  # update when
         self.recentSaves = []  # when written to program_state file, add - to beginning of each item in list
 
-        self.recurringIncomeDict = {}
+        self.recurringIncomeDict = {} # each item is a list where 0 is
         self.recurringExpenseDict = {}
 
-        self.budgetCategoryDict = {}  # budget categories and their budgets
+        self.budgetCategoryDict = {}  # budget categories and their balances
         self.budgetDict = {"default": {}}  # type of budget and each category's percentage
 
         self.moneyTypeDict = {
@@ -48,32 +60,42 @@ class Finance:
     def singularIncome(self):
         pass
 
-    def addRecurringIncome(self):
-        pass
+    def addRecurringIncome(self, id, moneyType, date, frequency):
+        self.recurringIncomeDict[id] = Recurring_Income(moneyType, date, frequency)
 
-    def removeRecurringIncome(self):
-        pass
+    def removeRecurringIncome(self, id):
+        self.recurringIncomeDict.pop(id)
 
 
     def singularExpense(self):
         pass
 
-    def addRecurringExpense(self):
-        pass
+    def addRecurringExpense(self, id, moneyType, date, frequency):
+        self.recurringExpenseDict[id] = Recurring_Income(moneyType, date, frequency)
 
-    def removeRecurringExpense(self):
-        pass
+    def removeRecurringExpense(self, id):
+        self.recurringExpenseDict.pop(id)
 
     #ie. divide income into different categories
-    def setBudget(self):
-        pass
+    def addBudgetCategory(self, id, balance):
+        self.budgetCategoryDict[id["balance"]] = balance
 
-    def addBudgetCategory(self):
-        pass
+    def removeBudgetCategory(self, id):
+        self.budgetCategoryDict.pop(id)
 
-    def removeBudgetCategory(self):
-        pass
+    def setPercentage(self, id, percentage):
+        self.budgetCategoryDict[id["percentage"]] = percentage
 
+    def addBudget(self, id, percentages):
+        #percentages is a dictionary listing each budget category and a corresponding percentage
+        self.budgetDict[id] = percentages
+
+    def removeBudget(self, id):
+        self.budgetDict.pop(id)
+
+    def applyBudget(self, id):
+        for category in self.budgetCategoryDict:
+            self.budgetCategoryDict[category["percentage"]] = self.budgetDict[id["percentage"]]
 
     #ie. gift card, bank account, paper money, ect.
     #find better method name
@@ -121,6 +143,33 @@ class Finance:
 
     def saveAs(self, address, fileName):
         save = open("{}/{}".format(address, fileName))
+
+        #save recurringIncomeDict
+        save.write("recurringIncomeDict")
+        for i in self.recurringIncomeDict:
+            save.write("-{}".format(i))
+            save.write("--{}".format(self.recurringIncomeDict.get(i).moneyType))
+            save.write("--{}".format(self.recurringIncomeDict.get(i).date))
+            save.write("--{}".format(self.recurringIncomeDict.get(i).frequency))
+
+        #save recurringExpenseDict
+        save.write("recurringExpenseDict")
+        for i in self.recurringExpenseDict:
+            save.write("-{}".format(i))
+            save.write("--{}".format(self.recurringExpenseDict.get(i).moneyType))
+            save.write("--{}".format(self.recurringExpenseDict.get(i).date))
+            save.write("--{}".format(self.recurringExpenseDict.get(i).frequency))
+
+        #save budgetCategoryDict
+        for i in self.budgetCategoryDict:
+            pass
+        #save budgetDict
+
+        #save moneyTypeDict
+
+        #save goalDict
+
+
         save.close()
         #when saving dictionary use - to indicate level
         #ex.
