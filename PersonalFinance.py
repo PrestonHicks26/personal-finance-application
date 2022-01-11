2b13fb8231b4ea437c32a97ffd32dfb26c2f6663
Preston L. Hicks <hicksp@rider.edu>
2022-01-10 23:08:43 -0500

    Implementation of saving and loading profiles
    -deleted custom saving method
    -rewrote using .json format
    -added openSave variable
    -added saves list
    -modified recent saves to only hold three most recent saves
    -began writing close method
    -implemented singlular incomes and expenses
    -worked on implementing goal and budget methods

--- PersonalFinance.py
+++ PersonalFinance.py
@@ -1,4 +1,6 @@
 from os import path
+import json
+import datetime
 
 class Recurring_Income:
     def __init__(self, moneyType, date, frequency):
@@ -14,51 +16,36 @@ class Recurring_Expense:
 
 class Finance:
     def __init__(self):
-        self.program_state = ""
-        self.lastSave = ""  # update when
-        self.recentSaves = []  # when written to program_state file, add - to beginning of each item in list
+        self.lastSave = ''
+        self.saves = []
+        self.recentSaves = [] # when save is saved, remove it from list, and then append back to the end, if list gets too long, remove oldest save
+        self.openSave = ''
+        if path.exists("program_state.json"):
+            with open("program_state.json", "r") as f:
+                jsonList = json.load(f)
+            self.lastSave = jsonList[0]
+            self.saves = jsonList[1]
+            self.recentSaves = jsonList[2]
+            self.openSave = self.lastSave
 
-        self.recurringIncomeDict = {} # each item is a list where 0 is
-        self.recurringExpenseDict = {}
-
-        self.budgetCategoryDict = {}  # budget categories and their balances
-        self.budgetDict = {"default": {"balance": 0}, }  # type of budget and each category's percentage
-
-        self.moneyTypeDict = {
-            "Cash": 0,
-            "Bank Account": 0
-        }
-
-        self.goalDict = {}
-
-        self.date = [1, 1, 2000]
-
-        if path.exists("program_state.txt"):
-            program_state = open("program_state.txt", "r")
-            for line in program_state:
-                if "*lastSave:" in line:
-                    self.lastSave = line[10:].rstrip()  # removes asterisk and newline
-                if line[0] == "-":
-                    self.recentSaves.append(line[1:].rstrip())
-            program_state.close()
+        if not self.saves:
+            self.recurringIncomeDict = {} # each item is a list where 0 is
+            self.recurringExpenseDict = {}
+            self.fundDict = {}  # funds (ie. college fund, personal spending, savings, ect.) and their balances
+            self.budgetDict = {"default": {"balance": 0}, }  # type of budget and each category's percentage
+            self.moneyTypeDict = {
+                "Cash": 0,
+                "Bank Account": 0
+            }
+            self.goalDict = {}
+            self.forcedBudget = False
         else:
-            # * used to avoid bug where user gives file same name as variable name,
-            # since * can't be used in windows files
-            program_state = open("program_state.txt", "x")
-            program_state.write("*lastSave:\n")
-            program_state.write("*recentSaves:\n")
-            program_state.close()
+            self.loadSave()
 
 
-        #if save file exists:
-            #load most recent save file
-        #else:
-            #set initial values
-        #self.lastSaveFile
-
-
-    def singularIncome(self):
-        pass
+    def singularIncome(self, value, fund, moneyType):
+        self.fundDict[fund] += value
+        self.moneyTypeDict[moneyType] += value
 
     def addRecurringIncome(self, id, moneyType, date, frequency):
         self.recurringIncomeDict[id] = Recurring_Income(moneyType, date, frequency)
@@ -67,8 +54,9 @@ class Finance:
         self.recurringIncomeDict.pop(id)
 
 
-    def singularExpense(self):
-        pass
+    def singularExpense(self,value, fund, moneyType):
+        self.fundDict[fund] -= value
+        self.moneyTypeDict[moneyType] -= value
 
     def addRecurringExpense(self, id, moneyType, date, frequency):
         self.recurringExpenseDict[id] = Recurring_Income(moneyType, date, frequency)
@@ -77,14 +65,14 @@ class Finance:
         self.recurringExpenseDict.pop(id)
 
     #ie. divide income into different categories
-    def addBudgetCategory(self, id, balance):
-        self.budgetCategoryDict[id] = {"balance": balance}
+    def addFund(self, id, balance):
+        self.fundDict[id] = {"balance": balance}
 
-    def removeBudgetCategory(self, id):
-        self.budgetCategoryDict.pop(id)
+    def removeFund(self, id):
+        self.fundDict.pop(id)
 
     def setPercentage(self, id, percentage):
-        self.budgetCategoryDict[id]["percentage"] = percentage
+        self.fundDict[id]["percentage"] = percentage
 
     def addBudget(self, id, percentages):
         #percentages is a dictionary listing each budget category and a corresponding percentage
@@ -94,8 +82,17 @@ class Finance:
         self.budgetDict.pop(id)
 
     def applyBudget(self, id):
-        for category in self.budgetCategoryDict:
-            self.budgetCategoryDict[category]["percentage"] = self.budgetDict[id["percentage"]]
+        if self.forcedBudget:
+            print("One of your goals has you on a forced budget, are you sure you want to change it?")
+            # spawn pop up box with yes and no buttons
+            # temp code:
+            if input() == 'y':
+                for category in self.fundDict:
+                    self.fundDict[category]["percentage"] = self.budgetDict[id["percentage"]]
+        else:
+            for category in self.fundDict:
+                self.fundDict[category]["percentage"] = self.budgetDict[id["percentage"]]
+
 
     #ie. gift card, bank account, paper money, ect.
     #find better method name
@@ -107,119 +104,99 @@ class Finance:
     def addMoneySubType(self, parent, id, balance):
         # adds an instance of a money type, creating a subdictionary
         #ex. visa gift card exp. 6/24
-        #ex.self.moneyTypeDict["Gift Cards"] = {vida gift card: 100}
+        #ex.self.moneyTypeDict["Gift Cards"] = {visa gift card: 100}
         self.moneyTypeDict[parent][id] = balance
         self.moneyTypeDict[parent]["total"] += balance
 
-    def addGoal(self, id, goal, dueDate, budget):
+    def addGoal(self, goal, value, dueDate, budget=None):
         #can call setBudget method
-        self.goalDict[id] = {"goal": goal, "dueDate": dueDate, "budget": budget}
+        self.goalDict[goal] = {"value": value, "dueDate": dueDate, "budget": budget}
+        if budget is not None:
+            self.applyBudget(budget)
+            self.forcedBudget = True
+        for goal in self.goalDict:
+            if self.goalDict[goal]['budget'] != budget and self.goalDict[goal]['budget'] is not None:
+                print("This goal's budget conflicts with another goal's budget. Which would you like to keep?")
+                # open pop up window and display 'old' on one button and 'new' on the other
+                # if new pressed:
+                    # applyBudget(budget)
 
     def removeGoal(self, id):
         self.goalDict.pop(id)
+        self.forcedBudget = False
+        for goal in self.goalDict:
+            if self.goalDict[goal]['budget'] is not None:
+                self.forcedBudget = True
 
     def checkProgress(self):
-        pass
+        date = datetime.datetime.now()
+        for goal in self.goalDict:
+            if date.year
 
-
-    def sendAlert(self, alertName):
-        if alertName == "":
+    def notification(self, notification):
+        if notification == "":
             pass
-        if alertName == "":
+        if notification == "":
             pass
 
 
     def saveProgramState(self):
-        program_state = open("program_state.txt", "w")
-        program_state.write("*lastSave:{}\n".format(self.lastSave))
-        program_state.write("*recentSaves:\n")
-        for save in self.recentSaves:
-            program_state.write("-{}".format(save))
-        program_state.close()
+        jsonList = [self.lastSave, self.saves, self.recentSaves]
+        with open('program_state.json', 'w') as f:
+            json.dump(jsonList, f)
 
     def save(self):
-        if not (self.recentSaves or self.lastSave):
-            self.saveAs()
+        self.lastSave = self.openSave
+        if not (self.saves or self.lastSave):
+            print("Address: ")
+            address = input()
+            print("File Name: ")
+            fileName = input()
+            self.saveAs(address, fileName)
 
-        save = open(self.lastSave)
-        save.write("recurringIncomeDict\n")
-        for i in self.recurringIncomeDict:
-            save.write("-{}".format(i))
-            save.write("--{}".format(self.recurringIncomeDict.get(i).moneyType))
-            save.write("--{}".format(self.recurringIncomeDict.get(i).date))
-            save.write("--{}".format(self.recurringIncomeDict.get(i).frequency))
-
-        save.write("recurringExpenseDict\n")
-        for i in self.recurringExpenseDict:
-            save.write("-{}".format(i))
-            save.write("--{}".format(self.recurringExpenseDict.get(i).moneyType))
-            save.write("--{}".format(self.recurringExpenseDict.get(i).date))
-            save.write("--{}".format(self.recurringExpenseDict.get(i).frequency))
-
-        save.write("budgetCategoryDict\n")
-        for i in self.budgetCategoryDict:
-            save.write("-{}:{}".format(i, self.budgetCategoryDict.get(i)))
-
-        save.write("budgetDict\n")
-        for i in self.budgetDict:
-            save.write("-{}:{}".format(i, self.budgetDict.get(i)))
-
-        save.write("moneyTypeDict\n")
-        for i in self.moneyTypeDict:
-            save.write("-{}:{}".format(i, self.moneyTypeDict.get(i)))
-
-        save.write("goalDict\n")
-        for i in self.goalDict:
-            save.write("-{}:{}".format(i, self.goalDict.get(i)))
-
-        save.close()
-        # when saving dictionary use - to indicate level of subdictionary
-        # ex.
-        # -trial1
-        # --distance
-        # --speed
-        # trial2
-        #when saving dictionary of objects use - to indicate attributes of object
-
+        jsonDict = {'recurringIncomeDict': self.recurringIncomeDict,
+                    'recurringExpenseDict': self.recurringExpenseDict,
+                    'fundDict': self.fundDict,
+                    'budgetDict': self.budgetDict,
+                    'moneyTypeDict': self.moneyTypeDict,
+                    'goalDict': self.goalDict,
+                    'forcedBudget': self.forcedBudget}
+        with open(self.lastSave, 'w') as f:
+            json.dump(jsonDict, f)
 
     def saveAs(self, address, fileName):
-        path = "{}\{}".format(address, fileName)
-        save = open(path)
-        self.lastSave = path
-        self.recentSaves.append(path)
-        self.save()
-
-
-
-    def loadSave(self, address):
-        save = open(address, "r")
-        current = ""
-
-        for line in save:
-            if "recurringIncomeDict" in line:
-                current = "recurringIncomeDict"
-            if "recurringExpenseDict" in line:
-                current = "recurringExpenseDict"
-            if "budgetCategoryDict" in line:
-                current = "budgetCategoryDict"
-            if "budgetDict" in line:
-                current = "budgetDict"
-            if "moneyTypeDict" in line:
-                current = "moneyTypeDict"
-            if "goalDict" in line:
-                current = "goalDict"
-            # load recurring income
-
-            #load recurring expense
-
-            #load budget categories
-
-            #load budgets
-
-            #load money types
-
-            #load goals
+        path = "{}\\{}.json".format(address, fileName)
+        save = open(path, 'w')
         save.close()
+        self.lastSave = path
+        self.saves.append(path)
+        self.recentSaves.remove(path)
+        self.recentSaves.append(path) # makes sure most recent save is at end of list
+        if len(self.recentSaves) > 3:
+            self.recentSaves.pop(0)
+
+    def loadSave(self, address): # rewrite to use .json format
+        self.openSave = address
+        self.recentSaves.remove(address)
+        self.recentSaves.append(address)  # makes sure most recent save is at end of list
+        if len(self.recentSaves) > 3:
+            self.recentSaves.pop(0)
+        with open(address, "r") as f:
+            jsonDict = json.load()
+            self.recurringIncomeDict = jsonDict['recurringIncomeDict']
+            self.recurringExpenseDict = jsonDict['recurringExpenseDict']
+            self.fundDict = jsonDict['fundDict']
+            self.budgetDict = jsonDict['budgetDict']
+            self.moneyTypeDict = jsonDict['moneyTypeDict']
+            self.goalDict = jsonDict['goalDict']
+            self.forcedBudget = jsonDict['forcedBudget']
+
+    #def update(self):
+
+    def close(self):
+        self.saveProgramState()
+        #if changes made w/o save, spawn confirmation box
+        #terminate window
 
 
     @staticmethod
